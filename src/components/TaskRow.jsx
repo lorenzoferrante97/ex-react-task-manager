1aec4a020d9a73e38b4b8f591d5f4e3e77bc9847
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-05-23 21:29:06 +0200

    add: colored status cells

--- src/components/TaskRow.jsx
+++ src/components/TaskRow.jsx
@@ -1,11 +1,17 @@
 export default function TaskRow({ task }) {
   const { title, status, createdAt } = task;
 
+  const statusStyles = {
+    todo: 'bg-red-200 text-red-700',
+    doing: 'bg-amber-200 text-amber-700',
+    done: 'bg-green-200 text-green-700',
+  };
+
   return (
     <>
       <div className="col-span-full grid grid-cols-subgrid gap-x-2 rounded-lg">
         <div className="col-span-4 p-2 bg-white rounded-lg">{title}</div>
-        <div className="col-span-4 p-2 bg-white rounded-lg">{status}</div>
+        <div className={`col-span-4 p-2 rounded-lg ${status == 'To do' ? statusStyles.todo : status == 'Doing' ? statusStyles.doing : statusStyles.done}`}>{status}</div>
         <div className="col-span-4 p-2 bg-white rounded-lg">{createdAt}</div>
       </div>
     </>
