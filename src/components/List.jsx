2928185dfc39abfb82d94894d883b42b5c191fef
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-05-23 21:12:13 +0200

    add: table

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -3,13 +3,26 @@ import { memo } from 'react';
 export default memo(function List({ tasks }) {
   return (
     <>
-      <ul>
+      {/* <ul>
         {tasks?.map((task) => {
           const { title, id } = task;
 
           return <li key={id}>{title}</li>;
         })}
-      </ul>
+      </ul> */}
+      <div className="w-full bg-neutral-100 p-3 grid grid-cols-12 gap-y-2 rounded-2xl">
+        {/* titles */}
+        <div className="col-span-full grid grid-cols-subgrid gap-x-2">
+          <div className="col-span-4 font-semibold text-lg p-2">Nome</div>
+          <div className="col-span-4 font-semibold text-lg p-2">Stato</div>
+          <div className="col-span-4 font-semibold text-lg p-2">Data Creazione</div>
+        </div>
+        <div className="col-span-full grid grid-cols-subgrid gap-x-2 rounded-lg">
+          <div className="col-span-4 p-2 bg-white rounded-lg">Nome</div>
+          <div className="col-span-4 p-2 bg-white rounded-lg">Stato</div>
+          <div className="col-span-4 p-2 bg-white rounded-lg">Data Creazione</div>
+        </div>
+      </div>
     </>
   );
 });
