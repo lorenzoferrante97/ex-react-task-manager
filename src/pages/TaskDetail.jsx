b3861565fe3a33122779d6ea2e76086421b523fb
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-05-27 09:18:50 +0200

    add: delete task button

--- src/pages/TaskDetail.jsx
+++ src/pages/TaskDetail.jsx
@@ -11,10 +11,15 @@ export default function TaskDetail() {
 
   const createdDate = new Date(createdAt).toLocaleString();
 
-  const statusStyles = {
-    todo: 'bg-red-200 text-red-700',
-    doing: 'bg-amber-200 text-amber-700',
-    done: 'bg-green-200 text-green-700',
+  const statusTextStyles = {
+    todo: 'text-red-700',
+    doing: 'text-amber-700',
+    done: 'text-green-700',
+  };
+  const statusDotStyles = {
+    todo: 'bg-red-300',
+    doing: 'bg-amber-300',
+    done: 'bg-green-300',
   };
 
   return (
@@ -22,8 +27,18 @@ export default function TaskDetail() {
       <span className="border border-neutral-200 text-neutral-500 rounded-sm p-2 w-fit">Task Detail {taskId}</span>
       <div className="w-full bg-neutral-100 p-3 grid grid-cols-12 gap-y-2 rounded-2xl">
         <div className="col-span-full bg-white p-4 rounded-lg flex f gap-3 items-center justify-between">
-          <h1 className="text-4xl font-bold">{title}</h1>
-          <span className={` py-2 px-3 rounded-lg text-lg font-semibold text-center ${status == 'To do' ? statusStyles.todo : status == 'Doing' ? statusStyles.doing : statusStyles.done} `}>{status}</span>
+          <div className="flex gap-4 items-center">
+            <h1 className="text-4xl font-bold">{title}</h1>
+            <div className={` flex items-center gap-2 ${status == 'To do' ? statusTextStyles.todo : status == 'Doing' ? statusTextStyles.doing : statusTextStyles.done}`}>
+              <span className={`inline-block size-2 rounded-full ${status == 'To do' ? statusDotStyles.todo : status == 'Doing' ? statusDotStyles.doing : statusDotStyles.done} `}></span>
+              <span className="text-lg font-semibold">{status}</span>
+            </div>
+          </div>
+          <div>
+            <button onClick={() => console.log('task eliminato')} className="hover:cursor-pointer bg-red-700 text-white rounded-lg px-4 py-2 font-semibold">
+              Elimina Task
+            </button>
+          </div>
         </div>
         <div className="flex flex-col col-span-full">
           <div className="col-span-full p-4 flex items-center gap-3">
