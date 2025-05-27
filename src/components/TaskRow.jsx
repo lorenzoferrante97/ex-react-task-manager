e3c38fe9586e9747f363d174491dda849376af3c
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-05-27 07:50:21 +0200

    edit: task title as Link

--- src/components/TaskRow.jsx
+++ src/components/TaskRow.jsx
@@ -1,7 +1,8 @@
 import { memo } from 'react';
+import { Link } from 'react-router-dom';
 
 export default memo(function TaskRow({ task }) {
-  const { title, status, createdAt } = task;
+  const { title, status, createdAt, id } = task;
   const createdDate = new Date(createdAt).toLocaleString();
 
   const statusStyles = {
@@ -13,7 +14,9 @@ export default memo(function TaskRow({ task }) {
   return (
     <>
       <div className="col-span-full grid grid-cols-subgrid gap-x-2 rounded-lg">
-        <div className="col-span-7 p-2 bg-white rounded-lg">{title}</div>
+        <div className="col-span-7 p-2 bg-white rounded-lg hover:cursor-pointer">
+          <Link to={`/task/:${id}`}>{title}</Link>
+        </div>
         <div className={`col-span-2 p-2 rounded-lg ${status == 'To do' ? statusStyles.todo : status == 'Doing' ? statusStyles.doing : statusStyles.done}`}>{status}</div>
         <div className="col-span-3 p-2 bg-white rounded-lg">{createdDate}</div>
       </div>
