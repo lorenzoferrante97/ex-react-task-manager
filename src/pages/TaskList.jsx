d8f202066eeb02a537cd3897c4fd22d867e3dc84
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-06-03 09:03:45 +0200

    add: input search task by title

--- src/pages/TaskList.jsx
+++ src/pages/TaskList.jsx
@@ -7,6 +7,12 @@ export default function TaskList() {
   return (
     <>
       <h1 className="text-4xl font-bold text-center">Task List</h1>
+      <div className="flex flex-col gap-3">
+        <label className="text-lg" htmlFor="searchTask">
+          Cerca una task per nome
+        </label>
+        <input className="w-full rounded-lg px-4 py-2 min-h-12 bg-white border border-neutral-200 shadow-2xl shadow-black/10" type="text" placeholder="Fare la spesa..." name="searchTask" id="searchTask" />
+      </div>
       {tasks?.length != 0 ? <List tasks={tasks} /> : <p>No Tasks</p>}
     </>
   );
